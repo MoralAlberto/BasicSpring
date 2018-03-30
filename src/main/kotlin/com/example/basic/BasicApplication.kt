4cdc4ecb0c0126e5345231306958ed79245f4cbf
Alberto Moral <alberto.moral.g@gmail.com>
2018-03-30 21:01:07 +0200

    Add POST customer

--- src/main/kotlin/com/example/basic/BasicApplication.kt
+++ src/main/kotlin/com/example/basic/BasicApplication.kt
@@ -15,8 +15,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.support.TransactionTemplate
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.servlet.view.script.ScriptTemplateConfig
 import org.springframework.web.servlet.view.script.ScriptTemplateConfigurer
@@ -30,22 +29,6 @@ fun main(args: Array<String>) {
     SpringApplicationBuilder()
             .sources(BasicApplication::class.java)
             .initializers(beans {
-
-                bean {
-                    ScriptTemplateViewResolver().apply {
-                        this.setSuffix(".kts")
-                        this.setPrefix("templates/")
-                    }
-                }
-
-                bean {
-                    ScriptTemplateConfigurer().apply {
-                        setScripts("scripts/render.kts")
-                        this.engineName = "kotlin"
-                        this.renderFunction = "render"
-                        this.isSharedEngine = false
-                    }
-                }
 
                 bean {
                     SpringTransactionManager(ref())
@@ -68,12 +51,15 @@ fun main(args: Array<String>) {
 class CustomerRestController(private val customerService: CustomerService) {
     @GetMapping("customers")
     fun customers() = customerService.all()
-}
 
-@Controller
-class CustomerController(private val customerService: CustomerService) {
-    @GetMapping("customers.php")
-    fun customers() = ModelAndView("customers", mapOf("customers" to customerService.all()))
+    @GetMapping("customer/{id}")
+    fun customers(@PathVariable id: Long) = customerService.byId(id)
+
+    @PostMapping("new")
+    fun customers(@RequestBody customer: Customer) {
+        val newCustomer = Customer(customer.name)
+        customerService.insert(newCustomer)
+    }
 }
 
 object Customers: Table() {
@@ -90,7 +76,8 @@ class ExposedCustomerService(private val transactionTemplate: TransactionTemplat
         }
     }
 
-    override fun all(): Collection<Customer> = Customers.selectAll().map { Customer(it[Customers.name], it[Customers.id]) }
+    override fun all(): Collection<Customer> =
+            Customers.selectAll().map { Customer(it[Customers.name], it[Customers.id]) }
 
     override fun byId(id: Long): Customer? =
             Customers
@@ -99,27 +86,7 @@ class ExposedCustomerService(private val transactionTemplate: TransactionTemplat
                     .firstOrNull()
 
     override fun insert(c: Customer) {
-        Customers.insert { it[Customers.name] = c.name }
-    }
-}
-
-@Profile("jdbc")
-@Service
-@Transactional
-class JdbcCustomerService(private val jdbcOperations: JdbcOperations): CustomerService {
-    override fun all(): Collection<Customer> = this.jdbcOperations.query("SELECT * FROM CUSTOMERS") { rs, _ ->
-        Customer(rs.getString("NAME"), rs.getLong("ID"))
-    }
-
-    override fun byId(id: Long): Customer? = this.jdbcOperations.queryForObject("SELECT * FROM CUSTOMERS where ?", id) { rs, _ ->
-        Customer(rs.getString("NAME"), rs.getLong("ID"))
-    }
-
-    override fun insert(c: Customer) {
-        this.jdbcOperations.execute("INSERT into CUSTOMERS (NAME) values(?)") {
-            it.setString(1, c.name)
-            it.execute()
-        }
+            Customers.insert { it[Customers.name] = c.name }
     }
 }
 
